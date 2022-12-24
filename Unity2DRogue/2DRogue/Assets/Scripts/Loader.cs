d1284321f9121391ce391717248c6d423832c47b
GiCheols <ngc4665@naver.com>
2022-12-24 19:31:05 +0900

    Unity를 사용한 2D 로그라이크 게임 Uploaded

---
+++ Unity2DRogue/2DRogue/Assets/Scripts/Loader.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Loader : MonoBehaviour
+{
+    public GameObject gameManager;
+    public GameObject soundManager;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        if (GameManager.instance == null)
+            Instantiate(gameManager);
+
+        if (SoundManager.instance == null)
+            Instantiate(soundManager);
+    }
+}
